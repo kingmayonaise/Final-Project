4b94c4d65dcf9cc23bb7abce60e0b9dc9da3a3e2
kingmayonaise <danielmelnikov@hanovernorwichschools.org>
2016-12-19 13:49:37 -0500

    Updated from Brython Server: 12/19/2016 1:49:36 PM

--- FinalProject.py
+++ FinalProject.py
@@ -1,1 +1,22 @@
+'''
+Final Project
+'''
+from collections import Counter
+from math import floor
+from ggame import App, Color, LineStyle, Sprite, RectangleAsset, CircleAsset, EllipseAsset, PolygonAsset, ImageAsset, MouseEvent, Frame
 
+red = Color(0xff0000, 1.0)
+green = Color(0x00ff00, 1.0)
+black = Color(0x000000, 1.0)
+white=Color(0xffffff,1.0)
+yellow=Color(0xffff00,1.0)
+
+thinline = LineStyle(1, black)
+noline=LineStyle(0,white)
+
+class Background(App):
+    def __init__(self):
+        RectangleAsset(noline,black)
+
+myapp=Background()
+myapp.run()
