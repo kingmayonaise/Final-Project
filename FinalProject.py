52523a26a8b9fad54520d5a1d05035e7b4b0c1bd
kingmayonaise <danielmelnikov@hanovernorwichschools.org>
2016-12-23 16:30:09 -0500

    Updated from Brython Server: 12/23/2016 4:30:09 PM

--- FinalProject.py
+++ FinalProject.py
@@ -14,19 +14,53 @@ yellow=Color(0xffff00,1.0)
 thinline = LineStyle(1, black)
 noline=LineStyle(0,white)
 
+
+
 class Pacman(Sprite):
-    #pacman=CircleAsset(5,noline,yellow)
-    pacman=RectangleAsset(20,10,noline,yellow)
+    pacman=CircleAsset(10,noline,yellow)
+    #pacman=RectangleAsset(20,10,noline,yellow)
     def __init__(self,position):
         super().__init__(Pacman.pacman,position)
-        self.vx=1
-        self.vy=1
-        self.vr=0.01
+        self.vx=0
+        self.vy=0
+        self.vr=0
         self.moving=0
         self.movingframe=1
-        PacmanGame.listenKeyEvent("keyleft", self.ismoving)
-        PacmanGame
+        '''
+        When keys are pressed
+        '''
         
+        PacmanGame.listenKeyEvent("keydown","right arrow", self.rightmoving)
+        PacmanGame.listenKeyEvent("keydown","left arrow",self.leftmoving)
+        PacmanGame.listenKeyEvent("keydown","down arrow",self.downmoving)
+        PacmanGame.listenKeyEvent("keydown","up arrow",self.upmoving)
+        '''
+        When keys are released
+        '''
+
+    '''
+    When keys are pressed they do these things
+    '''
+    
+    def rightmoving(self,event):
+            self.vx=1
+            self.vy=0
+            
+    def leftmoving(self,event):
+            self.vx=-1
+            self.vy=0
+            
+    def downmoving(self,event):
+            self.vy=1
+            self.vx=0
+    def upmoving(self,event):
+            self.vy=-1
+            self.vx=0
+            
+    '''
+    When keys are released they do these things
+    '''
+    
     def step(self):
         self.x+=self.vx
         self.y+=self.vy
@@ -39,14 +73,20 @@ class PacmanGame(App):
         bg=Sprite(background, (0,0))
         
     def step(self):
-        for pman in self.geSpritesbyClass(Pacman):
+        for pman in self.getSpritesbyClass(Pacman):
             pman.step()
         
+        
+class OtherSprites(Sprite):
+    def __init__(self):
+        wall=RectangleAsset(10,200,noline,white)
+        wallsprite=Sprite(wall, (200,25))
         
 
 myapp=PacmanGame(700,500) #Needs to go first so sprites show on top of it
 
 Pacman((100,100))
+OtherSprites()
 myapp.run()
 
 
